6126034aa4e49902caf9ae2c829a117b98f5a23a
chaliexe <lazaronfischer@gmail.com>
2023-09-11 17:22:06 -0300

    1.0.5

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CalendarComponent } from './calendar/calendar.component';
 import { AppComponent } from './app.component';
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
+import { HomeComponent } from './home/home.component';
+import { ShiftComponent } from './shift/shift.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    HomeComponent,
+    ShiftComponent
   ],
   imports: [
     BrowserModule,
