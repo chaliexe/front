86017080ac5b93fa29f709266cd26814d43b8471
chaliexe <lazaronfischer@gmail.com>
2023-09-13 18:51:15 -0300

    1.0.6

--- PFfront/src/app/app.component.spec.ts
+++ PFfront/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
+  it(`should have as title 'PFfront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
+    expect(app.title).toEqual('PFfront');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PFfront app is running!');
   });
 });
