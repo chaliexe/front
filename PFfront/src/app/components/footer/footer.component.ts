c137f666a7095cfefefff8c5cc58a035e4620e40
chaliexe <lazaronfischer@gmail.com>
2023-10-02 21:28:12 -0300

    1.1.3

--- PFfront/src/app/components/footer/footer.component.ts
+++ PFfront/src/app/components/footer/footer.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  redirectToIg(){
+    window.open('https://www.instagram.com/' )
+  }
+  redirectToFb(){
+    window.open('https://www.facebook.com/?locale=es_LA')
+  }
+
   redirectToWpp() {
     window.open('https://wa.me/+5493462548527', '_blank')
 
